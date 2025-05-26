278eeabc269774f6a4a40dd5db4e50d5007e0c6d
Charles Bousseau <cbousseau@anaconda.com>
2025-05-26 15:53:30 -0400

    only set SETUPTOOLS_SCM_PRETEND_VERSION when CONDA_BUILD=1

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -1,1 +1,4 @@
-set "SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION%"
+if "%CONDA_BUILD%"=="1" (
+    echo conda package setuptools_scm activate script: setting SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION% when CONDA_BUILD=1
+    set "SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION%"
+)
--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,1 +1,4 @@
-export SETUPTOOLS_SCM_PRETEND_VERSION=${PKG_VERSION}
+if [ "${CONDA_BUILD}" = "1" ]; then
+    echo "conda package setuptools_scm activate script: setting SETUPTOOLS_SCM_PRETEND_VERSION=${PKG_VERSION} when CONDA_BUILD=1"
+    export SETUPTOOLS_SCM_PRETEND_VERSION=${PKG_VERSION}
+fi
